b61c009821608bfd31fefd6e925094676a38f92a
Jennifer Li <ldnjennifer@hotmail.com>
2016-03-04 19:54:42 -0800

    defining user controller actions and adding view files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  
 end
