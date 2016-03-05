b8eedc5fab969442a7d28775e070100f3691da3e
Stanley Hansen <stanley321@gmail.com>
2016-03-04 20:30:51 -0800

    Added action to SkillsController

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -12,4 +12,18 @@
 #
 
 class SkillsController < ApplicationController
+  def new
+  end
+  def create
+  end
+  def index
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def destory
+  end
 end
