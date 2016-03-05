c3f9857e6fbacac04b4feb02d4b83fcc669a593e
Mark Alberton <mark.alberton@gmail.com>
2016-03-04 20:46:46 -0800

    add company and employment model validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ActiveRecord::Base
+  has_many :employments, dependent: :nullify
+  has_many :users, through: :employments
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
