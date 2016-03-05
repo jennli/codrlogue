4f8711aedf1647d439c0ea38d13fe493ef4b92fc
Ryan Kayu Ching <ryanching@hotmail.com>
2016-03-04 20:32:43 -0800

    Added actions and added has_many for Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,4 +9,29 @@
 #
 
 class CategoriesController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  def
+
+  def destroy
+
+  end
+
 end
