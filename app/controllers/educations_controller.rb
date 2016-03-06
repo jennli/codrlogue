709c505ff81c903483fb7e0419e90b0077351c64
Stanley Hansen <stanley321@gmail.com>
2016-03-05 18:20:04 -0800

    Rebase

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -14,4 +14,62 @@
 #
 
 class EducationsController < ApplicationController
+before_action :find_education, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @education = Education.new
+  end
+
+  def create
+    @education = Education.new education_params
+    @education.user = current_user
+    respond_to do |format|
+      if @education.save
+        format.js { render :education_create_success }
+      else
+        format.js { render :education_create_failure }
+      end
+    end
+  end
+
+  def index
+    @educations = current_user.educations
+  end
+
+  def show
+    respond_to do |format|
+      format.json { render json: @education.to_json }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @education.update education_params
+        format.json { render json: @education.to_json }
+      else
+        format.json { render json: @education.errors }
+      end
+    end
+  end
+
+  def destory
+    @education.user = current_user
+    @education.destroy
+    respond_to do |format|
+      format.json { render :education_destroy }
+    end
+  end
+
+  private
+
+  def find_education
+    @education = Education.find params[:id]
+  end
+
+  def education_params
+    params.require(:education).permit(:school_name,:school_link,:grade_year,:level,:field)
+  end
 end
