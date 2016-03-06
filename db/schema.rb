5541a493922bd2de1912df72e3b15184496b6f8e
Gitica Makhnotra <geetika9@hotmail.com>
2016-03-05 20:19:30 -0800

    added resume and image files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306025639) do
+ActiveRecord::Schema.define(version: 20160306040508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,7 @@ ActiveRecord::Schema.define(version: 20160306025639) do
     t.string   "github"
     t.string   "twitter"
     t.string   "image"
+    t.string   "attachment"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
