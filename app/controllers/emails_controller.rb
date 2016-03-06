5070427b51399386fa1e0c9fbe9dadb2c2aea89c
Tom <tomhung87@gmail.com>
2016-03-05 18:03:07 -0800

    Add email and body validations to emails_controller. Implement bootstrap styles in emails show page

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,14 +1,36 @@
 class EmailsController < ApplicationController
 
   def show
-    
+    find_user
   end
 
   def send_email
-    @user = User.friendly.find(params[:id])
+    find_user
     employer_params = params.permit(:email, :body)
-    ContactsMailer.notify_user(employer_params, @user).deliver_now
-    redirect_to user_path(@user), notice: "An email has been sent!"
+    @email = employer_params[:email]
+    @message = employer_params[:body]
+
+    if valid_email?(@email, @message)
+      ContactsMailer.notify_user(employer_params, @user).deliver_now
+      redirect_to user_path(@user), notice: "An email has been sent!"
+    else
+      flash[:alert] = "Please input a valid email and message"
+      render :show
+    end
   end
+
+
+  private
+
+  def find_user
+    @user = User.friendly.find(params[:id])
+  end
+
+  def valid_email?(email, message)
+    valid_regex = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    email.present? && (email =~ valid_regex) && message.present?
+  end
+
+
 
 end
