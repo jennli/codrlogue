1d357f071f7a9459e5be8cbdd1ed5f4ad17c3226
Jennifer Li <ldnjennifer@hotmail.com>
2016-03-05 16:30:57 -0800

    adding authenticate user check before render edit and edit password

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,16 +4,20 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-       user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-      end
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
 
-      can :manage, User do |owner|
-        user == owner
-      end
+    can :manage, User do |owner|
+      user == owner
+    end
+
+    can :view, User do |owner|
+      owner.approved || user.admin
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
