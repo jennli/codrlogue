980ba09d7d8bea62799303a184cea2615af0d161
Mark Alberton <mark.alberton@gmail.com>
2016-03-06 21:12:15 -0800

    Photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,6 +94,11 @@ class UsersController < ApplicationController
 
   def update
     @user.slug = nil
+    binding.pry
+    if params[:user][:remove_image]
+      @user.remove_image!
+      @user.save
+    end
     if @user.update user_params
       if user_params.has_key?(:approved) || user_params.has_key?(:admin)
         flash[:notice] = "Update success from Administrator"
